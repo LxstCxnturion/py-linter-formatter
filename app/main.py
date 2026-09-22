e3e83afc6a3d1b60811a5416877d268e9831d232
LxstCxnturion <sinitenkoffroman@gmail.com>
2026-09-22 18:20:40 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-def format_linter_error(error: dict) -> dict:
+from typing import Any
+
+
+def format_linter_error(error: dict[str, Any]) -> dict[str, Any]:
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -7,17 +10,22 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8",
     }
 
-def format_single_linter_file(file_path: str, errors: list) -> dict:
+
+def format_single_linter_file(
+    file_path: str,
+    errors: list[dict[str, Any]],
+) -> dict[str, Any]:
     return {
-        "path": path,
+        "path": file_path,
         "status": "failed" if len(errors) > 0 else "passed",
         "errors": [format_linter_error(error) for error in errors],
     }
 
 
-
-def format_linter_report(linter_report: dict) -> list:
+def format_linter_report(
+    linter_report: dict[str, list[dict[str, Any]]],
+) -> list[dict[str, Any]]:
     return [
-        format_single_linter_file(path, errors)
-        for path, errors in report.items()
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
     ]
